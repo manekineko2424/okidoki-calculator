5836e027014f096a8b5cf3451bb4b3c8e15d814f
manekineko <taku.app.dev@gmail.com>
2026-01-13 01:27:52 +0900

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by Ohku takuya on 2026/01/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
